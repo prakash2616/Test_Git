764cb23591bf599f0c210cd76258a765d02925b8
prakash <prajeshprajesh477@gmail.com>
2023-08-03 14:14:50 +0530

    Changes made to feature branch

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 class Test {
     public static void main(String[] ss){
         System.out.println("Hello World in master branch");
+        System.out.println("How are you");
+        System.out.println("Changes made in feature");
     }
 }
